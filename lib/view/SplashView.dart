991aeac2e916f4ae50d0e08dd716ab12443e8ece
ts.Ryu <lts06069@naver.com>
2026-04-06 10:50:29 +0900

    기능 최적화 및 꾸미기 - 2
    기능 최적화 및 꾸미기 - 2

--- lib/view/SplashView.dart
+++ lib/view/SplashView.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart'; // Lottie 임포트
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // 2초 뒤 메인으로 이동
-    Future.delayed(Duration(seconds: 2), () => Get.offNamed('/home'));
+    // 3초 뒤 메인으로 이동
+    Future.delayed(Duration(seconds: 3), () => Get.offNamed('/home'));
 
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.account_balance_wallet, size: 100, color: Colors.blue),
+            Lottie.asset(
+              'assets/lottie/money.json',
+              width: 250,
+              height: 250,
+              fit: BoxFit.fill,
+            ),
             SizedBox(height: 20),
-            Text("나의 용돈기입장", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            Text(
+              "똑똑한 용돈 대장",
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.2,
+              ),
+            ),
           ],
         ),
       ),
